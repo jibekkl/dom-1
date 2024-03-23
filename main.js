011bddbfec1f4769e0aaa8a43eedfcbc2d45f7a2
Your Name <your_email@example.com>
2024-03-23 13:03:51 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+console.log(this);
+// console.log(window);
+
+console.log(document);
+////// выводит свой html
+
+// const title=document.getElementsByClassName(".title")
+//////////////////////////////////////////////////////////
+// const h1 = document.querySelector("h1");
+// h1.style.color = "purple";
+// h1.style.fontFamily = "san-serif";
+// h1.style.letterSpacing = "2px";
+// console.log(h1);
+
+// const title = document.querySelectorAll(".title");
+// title[0].style.color = "red";
+// console.log(title);
+
+// const hero = document.querySelectorAll("#hero");
+// hero[0].style.width = "300px";
+// hero[0].style.height = "300px";
+// hero[0].style.background = "grey";
+// hero[0].style.textAlign = "center";
+// console.log(h
+
+
+const block = document.querySelector(".block");
+block.style.width = "300px";
+block.style.height = "400px";
+block.style.marginLeft = "500px";
+block.style.marginTop = "40px";
+block.style.background = "purple";
+block.style.display = "flex";
+block.style.flexDirection = "column";
+block.style.justifyContent = "center";
+block.style.gap = "20px";
+block.style.alignItems = "center";
+block.style.borderRadius = "30px";
+
+const img = document.querySelector("img");
+img.style.width = "100px";
+
+const input1 = document.querySelector(".input1");
+input1.style.width = "250px";
+input1.style.height = "20px";
+input1.style.borderRadius = "10px";
+input1.style.border = "none";
+
+const input2 = document.querySelector(".input2");
+input2.style.width = "150px";
+input2.style.height = "20px";
+input2.style.borderRadius = "10px";
+input2.style.border = "none";
+
+const button = document.querySelector("button");
+button.style.width = "80%";
+button.style.height = "50px";
+button.style.marginBottom = "20px";
+button.style.color = "white";
+button.style.background = "orange";
+button.style.borderRadius = "15px";
+
+const h1 = document.querySelector("h1");
+h1.style.fontFamily = "san-serif";
+h1.style.border = "none";
+
+const body = document.querySelector("body");
+body.style.backgroundColor = "white";
